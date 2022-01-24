56a3adec47fd692be99d60c98964b82b2fc0a8fa
Dan <panda.gamer2004@gmail.com>
2022-01-24 12:37:24 +0200

    Changed: ChangeEntitiesInDb Added method MakeAndExecuteQueryForEachEntity

--- CustomORM/OrmLogic/DbEntitySet.cs
+++ CustomORM/OrmLogic/DbEntitySet.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Linq.Expressions;
 using CustomORM.EventArgsObjects;
-using CustomORM.Exceptions;
 using CustomORM.Interfaces;
 
 namespace CustomORM.OrmLogic
@@ -12,36 +12,50 @@ namespace CustomORM.OrmLogic
     public class DbEntitySet<T> : IEntitySet<T>
         where T : class, new()
     {
-
         public DbEntitySet(SqlCommand command)
         {
             _command = command;
-        } 
-        private readonly Queue<String> _commandsToExecute = new();
+        }
+
         private SqlCommand _command;
         private readonly ICommandBuilder<T> _commandBuilder = new SimpleCommandBuilder<T>();
-        private readonly IModelSerializer<T> _modelSerializer = new ModelSerializer<T>() ;
+        private readonly IModelSerializer<T> _modelSerializer = new ModelSerializer<T>();
+
         private readonly IEntityStateTracker<T> _entityStateTracker = new EntityStateTracker<T>(
             new EntityCopyBuilder<T>(), new EntityEqualityComparer<T>());
-        
-        private T GetEntityForFind(String commandText)
+
+        private void MakeAndExecuteQueryForEachEntity(IEnumerable<T> entities, Func<T, QueryEntity> queryBuilder)
         {
-            _command.CommandText = commandText;
-        
-            using var reader = _command.ExecuteReader();
-                    
-            if (!reader.HasRows)
+            foreach (var entityToInsert in entities)
             {
-                throw new EntityNotFoundException();
+                var queryEntityForInsert = queryBuilder(entityToInsert);
+                _command.CommandText = queryEntityForInsert.QueryText;
+
+                if (queryEntityForInsert.CommandParams is not null)
+                {
+                    _command.Parameters.AddRange((SqlParameter[]) queryEntityForInsert.CommandParams);
+                }
+
+                _command.ExecuteNonQuery();
             }
-                    
-            reader.Read();
-            return _modelSerializer.SerializeRowToEntity(reader);
         }
-        
+
+        private void ChangeEntitiesInDb()
+        {
+            _entityStateTracker.CheckTrackedEntitiesToChangeState();
+
+            var entitiesToInsert = _entityStateTracker.GetEntitiesToAdd();
+            var entitiesToUpdate = _entityStateTracker.GetEntitiesToUpdate();
+            var entitiesToDelete = _entityStateTracker.GetEntitiesToDelete();
+            
+            MakeAndExecuteQueryForEachEntity(entitiesToInsert, _commandBuilder.GenerateInsertCommand);
+            MakeAndExecuteQueryForEachEntity(entitiesToUpdate, _commandBuilder.GenerateUpdateCommand);
+            MakeAndExecuteQueryForEachEntity(entitiesToDelete, _commandBuilder.GenerateDeleteCommand);
+        }
+
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException(); 
+            throw new NotImplementedException();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -51,35 +65,25 @@ namespace CustomORM.OrmLogic
 
         public void AddEntity(T entity)
         {
-            
-            
         }
 
         public void RemoveEntity(T entityToRemove)
         {
-            
         }
 
         public void AddRangeEntities(params T[] entitiesToAdd)
         {
-            
         }
 
 
-        
-        
         public T Find(int id)
         {
-            //TODO WRITE HERE
-            var commandText = _commandBuilder.GenerateSelectCommand(id);
-            return GetEntityForFind(commandText);
+            throw new NotImplementedException();
         }
 
         public T Find(Guid id)
         {
-            //TODO WRITE HERE
-            var commandText = _commandBuilder.GenerateSelectCommand(id);
-            return GetEntityForFind(commandText);
+            throw new NotImplementedException();
         }
 
         public void Include(string nameOfIncludeProperty)
@@ -93,19 +97,10 @@ namespace CustomORM.OrmLogic
         }
 
 
-        private void ExecuteAllCommands()
-        {
-            while (_commandsToExecute.TryDequeue(out var commandToExecute))
-            {
-                _command.CommandText = commandToExecute;
-                _command.ExecuteNonQuery();
-            }
-        }
-
         public void SetCurrentCommand(Object sender, CommandEventArgs commandEventArgs)
         {
             _command = commandEventArgs.Command;
-            this.ExecuteAllCommands();
+            this.ChangeEntitiesInDb();
         }
     }
 }
