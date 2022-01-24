b487c6c7e6b73d533e1b115089971b9a4107c804
Dan <panda.gamer2004@gmail.com>
2022-01-24 17:06:57 +0200

    add NavigationalPropertyNotFoundException

---
+++ CustomORM/Exceptions/NavigationalPropertyNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomORM.Exceptions
+{
+    public class NavigationalPropertyNotFoundException : Exception
+    {
+        public NavigationalPropertyNotFoundException(string message) : base(message)
+        {
+            
+        }
+    }
+}
