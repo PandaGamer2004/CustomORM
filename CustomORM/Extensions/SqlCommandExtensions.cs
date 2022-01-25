0f977bcb29f4a673bcae0d00426f0751b9ee0e22
Dan <panda.gamer2004@gmail.com>
2022-01-26 00:01:26 +0200

    removed ConsoleApp1.csproj

---
+++ CustomORM/Extensions/SqlCommandExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace CustomORM
+{
+    public static class SqlCommandExtensions
+    {
+        public static void AddParamsList(this SqlCommand command, IEnumerable<SqlParameter>? sqlParameters)
+        {
+            if(sqlParameters is null) return;
+
+            foreach (var sqlParameter in sqlParameters)
+            {
+                command.Parameters.Add(sqlParameter);
+            }
+        } 
+    }
+}
